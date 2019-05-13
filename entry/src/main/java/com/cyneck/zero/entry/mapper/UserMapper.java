f147d45c0a8a38624cfd89d60e07cfbb2e57d5d2
Eric <li114228@126.com>
2019-05-13 17:41:07 +0800

    代码生成插件

---
+++ entry/src/main/java/com/cyneck/zero/entry/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.cyneck.zero.entry.mapper;
+
+import com.cyneck.zero.entry.model.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
