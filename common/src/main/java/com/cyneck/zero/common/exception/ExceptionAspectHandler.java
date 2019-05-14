60d370807c9336f4880dbbe206d008000cad2d71
Eric <li114228@126.com>
2019-05-14 17:44:15 +0800

    全局异常捕获

--- common/src/main/java/com/cyneck/zero/common/exception/ExceptionAspectHandler.java
+++ common/src/main/java/com/cyneck/zero/common/exception/ExceptionAspectHandler.java
@@ -1,17 +1,18 @@
 package com.cyneck.zero.common.exception;
 
 import com.cyneck.zero.common.model.ApiResponse;
-import com.google.common.base.Stopwatch;
-import com.google.common.collect.Lists;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-import java.util.concurrent.TimeUnit;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Eric Lee
@@ -26,29 +27,43 @@ public class ExceptionAspectHandler {
 
     public static final Logger logger = LoggerFactory.getLogger(ExceptionAspectHandler.class);
 
-    @Pointcut("execution(* com.cyneck.zero.*.*(..))")//此处基于自身项目的路径做具体的设置
-    public void pointCut() {
+
+    @Pointcut("execution(* com.cyneck.zero..*(..))")//此处基于自身项目的路径做具体的设置
+    public void ExceptionPointcut() {
+/*        第一种：表示在UserMangeImpl类下的所有方法，这种方式在之前的案例使用的。
+        @Pointcut("execution(* com.spring.two.UserMangeImpl.*(..))")
+
+        第二种：表示任意公共的方法，其他修饰符也同样的道理。
+        @Pointcut("execution(public * *(..))")
+
+        第三种：任何一个以set开始的方法，不仅仅是set，save、update也都行。
+        @Pointcut("execution(* set*(..))")
+
+        第四种：在指定包下的所有方法
+        @Pointcut("execution(* com.spring.*.*(..))")
+*/
     }
 
-    @Around("pointCut()")
-    public Object handleControllerMethod(ProceedingJoinPoint pjp) {
-        Stopwatch stopwatch = Stopwatch.createStarted();
 
-        ApiResponse apiResponse = new ApiResponse();
-        try {
-            logger.info("执行Controller开始: " + pjp.getSignature() + " 参数：" + Lists.newArrayList(pjp.getArgs()).toString());
-            apiResponse = (ApiResponse) pjp.proceed(pjp.getArgs());
-            logger.info("执行Controller结束: " + pjp.getSignature() + "， 返回值：" + apiResponse.toString());
-            logger.info("耗时：" + stopwatch.stop().elapsed(TimeUnit.MILLISECONDS) + "(毫秒).");
-        } catch (Throwable throwable) {
-            //apiResponse = handlerException(pjp, throwable);
-        }
-
-        return apiResponse;
+    /**
+     * 后置最终通知,final增强，不管是抛出异常或者正常退出都会执行
+     */
+    @After("ExceptionPointcut()")
+    public void after(JoinPoint joinPoint) {
+        System.out.println("方法最后执行.....");
     }
 
-    private ApiResponse<?> handlerException(ProceedingJoinPoint pjp, Throwable e) {
-        ApiResponse<?> apiResponse = null;
+
+    /**
+     * 后置异常通知
+     */
+    @AfterThrowing(value = "ExceptionPointcut()", throwing = "error")
+    public ApiResponse throwsExcpetion(JoinPoint joinPoint, Throwable error) {
+        System.out.println("异常时执行.....");
+        ApiResponse apiResponse = new ApiResponse("400", "400", "error");
+
+        /*获得 request */
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         return apiResponse;
     }
 
