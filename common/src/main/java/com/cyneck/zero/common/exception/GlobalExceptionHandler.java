55086b2b4d59ef186a50547738ebc5a7265d81c0
Eric <li114228@126.com>
2019-05-24 18:03:36 +0800

    校验异常统一处理

--- common/src/main/java/com/cyneck/zero/common/exception/GlobalExceptionHandler.java
+++ common/src/main/java/com/cyneck/zero/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,9 @@ package com.cyneck.zero.common.exception;
 import com.cyneck.zero.common.model.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,6 +39,32 @@ public class GlobalExceptionHandler {
         ApiResponse response = new ApiResponse();
         response.setMessage("运行时异常！");
         return response;
+    }
+
+    /**
+     * 所有验证框架异常捕获处理
+     *
+     * @return
+     */
+    @ResponseBody
+    @ExceptionHandler(value = {BindException.class, MethodArgumentNotValidException.class})
+    public Object validationExceptionHandler(Exception exception) {
+        BindingResult bindResult = null;
+        if (exception instanceof BindException) {
+            bindResult = ((BindException) exception).getBindingResult();
+        } else if (exception instanceof MethodArgumentNotValidException) {
+            bindResult = ((MethodArgumentNotValidException) exception).getBindingResult();
+        }
+        String msg;
+        if (bindResult != null && bindResult.hasErrors()) {
+            msg = bindResult.getAllErrors().get(0).getDefaultMessage();
+            if (msg.contains("NumberFormatException")) {
+                msg = "参数类型错误！";
+            }
+        } else {
+            msg = "系统繁忙，请稍后重试...";
+        }
+        return new ApiResponse("500", msg);
     }
 
     @ExceptionHandler(Exception.class)
--- entry/src/test/java/com/cyneck/zero/entry/controller/UserControllerTest.java
+++ entry/src/test/java/com/cyneck/zero/entry/controller/UserControllerTest.java
@@ -1,10 +1,10 @@
 package com.cyneck.zero.entry.controller;
 
-import com.cyneck.zero.common.model.PageEntity;
+import com.alibaba.fastjson.JSONObject;
 import com.cyneck.zero.entry.ApplicationEntry;
-import com.cyneck.zero.entry.model.condition.UserCondition;
+import com.cyneck.zero.entry.model.vo.TenantVo;
 import com.cyneck.zero.entry.service.UserService;
-import com.github.pagehelper.Page;
+import com.mchange.io.FileUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,23 +12,37 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Import(ApplicationEntry.class)
 public class UserControllerTest {
 
+    //单元测试，租户有关测试数据的资源文件父级路径
+    private final String baseParh = "data/";
+
     @Resource
     UserService userService;
 
     @Test
-    public void getUserPage() {
-        UserCondition condition = new UserCondition();
-        condition.setPageNum(1);
-        condition.setPageSize(4);
-        condition.setOrderField("name");
-        condition.setOrderDirection("desc");
-        PageEntity page = userService.getPageByCondition(condition);
-        userService.getPageByCondition(condition);
+    public void getUserPage() throws IOException {
+        String fielPath = getClass().getClassLoader().getResource(baseParh + "test.json").getPath();
+        File file = new File(fielPath);
+        if (file.exists()) {
+            String json = FileUtils.getContentsAsString(file);
+            TenantVo tenantVo = JSONObject.parseObject(json, TenantVo.class);
+            System.out.println("success");
+
+//            UserCondition condition = new UserCondition();
+//            condition.setPageNum(1);
+//            condition.setPageSize(4);
+//            condition.setOrderField("name");
+//            condition.setOrderDirection("desc");
+//            PageEntity page = userService.getPageByCondition(condition);
+//            userService.getPageByCondition(condition);
+        }
+
     }
 }
